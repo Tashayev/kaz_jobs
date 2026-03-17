2c95b1e0276fd59240ce18b571821b98a92f2547
Tashayev <adil.t_92@mail.ru>
2026-03-17 12:18:17 +0500

    miss refresh return

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -31,12 +31,13 @@ const registerUser = async (req, res) => {
       { session },
     )
 
-    const { accessToken } = await generateToken(user)
+    const { accessToken, refreshToken } = await generateTokens(user._id)
     await session.commitTransaction()
     res.status(201).json({
       message: "User created successfully.",
       user: { id: user._id, username: user.username, email: user.email, role: user.role },
       accessToken,
+      refreshToken,
     })
   } catch (err) {
     session.abortTransaction()
@@ -70,11 +71,12 @@ const loginUser = async (req, res) => {
       return res.status(400).json({ message: "Incorrect password." })
     }
 
-    const { accessToken } = await generateToken(user)
+    const { accessToken, refreshToken } = await generateTokens(user._id)
     res.status(200).json({
       message: "User logged in successfully.",
       user: { id: user._id, username: user.username, email: user.email, role: user.role },
       accessToken,
+      refreshToken,
     })
   } catch (err) {
     return res.status(500).json({ message: "Internal server error." })
