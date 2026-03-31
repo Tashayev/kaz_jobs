073546e6fa5a7b3ba0342dc6697e5d297f7d156e
Tashayev <adil.t_92@mail.ru>
2026-03-31 12:47:17 +0500

    created settings, my jobs

--- front/app/profile/[section]/page.tsx
+++ front/app/profile/[section]/page.tsx
@@ -2,9 +2,16 @@
 
 import { use } from "react"
 import { notFound } from "next/navigation"
-import { FileText, UserCircle, Settings, Briefcase } from "lucide-react"
+import {
+  FileText,
+  UserCircle,
+  Briefcase,
+  Settings as SettingsIcon,
+} from "lucide-react"
 import PersonalDetails from "../../../features/jobs/components/PersonalDetails"
 import CreateJobs from "../../../features/jobs/components/CreateJobs"
+import MyJobs from "@/features/users/components/MyJobs"
+import Settings from "./Section"
 
 const SECTIONS = [
   {
@@ -23,28 +30,23 @@ const SECTIONS = [
     title: "My Jobs",
     icon: Briefcase,
     path: "my-jobs",
-    component: <div>My Jobs</div>
+    component: <MyJobs />,
   },
   {
     title: "Settings",
-    icon: Settings,
+    icon: SettingsIcon,
     path: "settings",
-    component: <div>Settings</div>
+    component: <Settings />,
   },
 ]
 
-const SectionPage = ({
-  params,
-}: {
-  params: Promise<{ section: string }>
-}) => {
+const SectionPage = ({ params }: { params: Promise<{ section: string }> }) => {
   const { section } = use(params)
   const item = SECTIONS.find((i) => i.path === section)
   if (!item) return notFound()
 
   return (
     <div>
-     
       <div className="flex items-center gap-3 mb-8">
         <div className="w-10 h-10 rounded-xl bg-blue-50 flex items-center justify-center">
           <item.icon size={18} className="text-blue-600" />
--- front/features/jobs/types.ts
+++ front/features/jobs/types.ts
@@ -1,4 +1,3 @@
-import { title } from "process"
 
 export type Job = {
   _id: string
